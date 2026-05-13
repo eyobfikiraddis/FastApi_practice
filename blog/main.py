d88815198abd63bfd0ff351f70792f6aa8f09b2f
Eyob Fikiraddi <163149873+eyobfikiraddis@users.noreply.github.com>
2026-05-13 23:21:48 +0300

    now deleted a row

--- blog/main.py
+++ blog/main.py
@@ -31,3 +31,17 @@ def create(request: schemas.Blog, db : Session = Depends(get_db)):
 def all(db:Session = Depends(get_db)):
     b = db.query(models.Blog).all()
     return b
+
+@app.get('/{id}')
+#to get those with the same id
+def getin(id, db : Session = Depends(get_db)):
+    b = db.query(models.Blog).filter(models.Blog.id == id).first() #to return only the fist one but if we wanted to return all we use .all()
+    return b
+
+@app.delete('/{id}')
+
+def delet(id, db : Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    db.commit()
+    return {'done'}
+    
